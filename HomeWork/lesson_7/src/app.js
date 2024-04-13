d314666e1fc67349dba60a145a6f60b9d06e6c70
vasiliyushin <vasiliyushin@yandex.ru>
2024-04-13 19:15:10 +0300

    добавил файлы по старым домашкам

---
+++ HomeWork/lesson_7/src/app.js
@@ -0,0 +1,16 @@
+const scores = {
+  Anna: 10,
+  Olga: 1,
+  Ivan: 5,
+  Vasiliy: 25
+};
+
+const getScore = (student) => {
+  let sumScores = 0;
+  for (let key in student) {
+    sumScores = sumScores + student[key]
+  }
+  console.log(sumScores)  
+}
+
+getScore(scores)
